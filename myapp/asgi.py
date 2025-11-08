1049eb3ddb1a2adf4f9731ad59dd0994cff70cdc
awatech <awatech12@gmail.com>
2025-11-08 03:51:34 -0800

    rearrange in asgi

--- myapp/asgi.py
+++ myapp/asgi.py
@@ -1,10 +1,11 @@
 
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
 from social import routing
-from django.core.asgi import get_asgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
